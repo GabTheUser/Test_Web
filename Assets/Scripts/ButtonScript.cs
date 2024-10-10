567e399aff80b8b5f4eeea4e07c1e48b678b06b6
Чабаев Джабраил <110449718+GabTheUser@users.noreply.github.com>
2024-10-11 00:25:07 +0300

    push2

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript: MonoBehaviour
+{
+    public Button uiButton; 
+    public LoadSprites imageLoader;
+    private Image buttonImage;
+
+    private float fadeDuration = 1.0f; 
+    private bool isFadingIn = true, buttonIsPressed;
+
+    private void Start()
+    {
+        buttonImage = uiButton.GetComponent<Image>(); 
+    }
+
+    public void ButtonPressed()
+    {
+        if (!buttonIsPressed)
+        {
+            imageLoader.Load();
+            StartCoroutine(FadeLoop());
+            buttonIsPressed = true;
+        } 
+    }
+
+    IEnumerator FadeLoop()
+    {
+        while (true)
+        {
+            float startAlpha = isFadingIn ? 0f : 1f;
+            float endAlpha = isFadingIn ? 1f : 0f;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float newAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeDuration);
+
+                Color color = buttonImage.color;
+                color.a = newAlpha;
+                buttonImage.color = color;
+
+                yield return null;
+            }
+
+            isFadingIn = !isFadingIn;
+        }
+    }
+}
---
+++ Assets/Scripts/LoadSprites.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+public class LoadSprites : MonoBehaviour 
+{
+    public Image[] images;         
+    public string atlasKey;       
+    public string[] spriteNames;  
+
+    public void Load()
+    {
+        StartCoroutine(LoadSpriteAtlas());
+    }
+
+    IEnumerator LoadSpriteAtlas()
+    {
+        var atlasLoadTask = Addressables.LoadAssetAsync<SpriteAtlas>(atlasKey);
+        yield return atlasLoadTask;
+
+        if (atlasLoadTask.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+        {
+            SpriteAtlas atlas = atlasLoadTask.Result;
+
+            for (int i = 0; i < images.Length; i++)
+            {
+                Sprite sprite = atlas.GetSprite(spriteNames[i]);
+
+                if (sprite != null)
+                {
+                    images[i].sprite = sprite;
+                }
+                else
+                {
+                    Debug.LogError($"Спрайт с именем {spriteNames[i]} не найден в атласе");
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Ошибка при загрузке атласа");
+        }
+    }
+}
